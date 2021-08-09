108845f008dc39a0b9d5c4702f0b90da58d02e33
lemjiamo <luismejia69@gmail.com>
2021-08-09 21:17:28 +0000

    fixeed line 4

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 /* CREATE USERS AND DATA BASES TO HOLBERTON SCHOOL PROJECT */
 CREATE database IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev@localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pw';
 GRANT all ON hbnb_dev_db to hbnb_dev@localhost;
 GRANT SELECT ON performance_schema . * TO 'hbnb_dev@localhost';
