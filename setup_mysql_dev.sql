64ac7231708f23cbd31312f2e12970884e5b617c
lemejiamo <luismejia69@gmail.com>
2021-08-09 20:39:00 -0500

    setup mysql dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,3 +3,4 @@ CREATE database IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pw';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* to hbnb_dev@localhost;
 GRANT SELECT ON performance_schema . * TO hbnb_dev@localhost;
+FLUSH PRIVILEGES;
