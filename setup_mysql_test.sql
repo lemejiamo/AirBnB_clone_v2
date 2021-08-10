b8bc098d03e3367291266b4dff75765997c0c7d5
lemejiamo <luismejia69@gmail.com>
2021-08-09 20:44:05 -0500

    setup serve to test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- CREATE USERS AND DATA BASES TO HOLBERTON SCHOOL PROJECT --
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* to hbnb_test@localhost;
+GRANT SELECT ON performance_schema . * TO hbnb_test@localhost;
+FLUSH PRIVILEGES;
