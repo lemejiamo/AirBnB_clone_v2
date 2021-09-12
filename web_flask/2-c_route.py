e5a23733bc5a3544aa46df5578ad3ad767ef4604
lemejiamo <luismejia69@gmail.com>
2021-09-12 00:40:59 -0500

    c route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Module to mange flask Server"""
+
+
+from re import split
+from flask  import Flask, render_template
+import subprocess
+
+
+app =  Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ route to root """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """" route to HBNB"""
+    return 'HBNB'
+
+@app.route('/c/<string_p>')
+def c_is_fun(string_p):
+    """ print args get it for url"""
+    string = string_p.split("_")
+    new_string = ""
+    for word in string:
+        new_string = new_string + " " + word
+    return new_string[1:]
+
+
+if __name__== "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
