e2b26a5237f05308eb97e10592addbd418be8a9c
lemejiamo <luismejia69@gmail.com>
2021-09-12 01:16:00 -0500

    number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+""" Module to mange flask Server"""
+
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ return index"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """" return HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<string_p>')
+def c_is_fun(string_p):
+    """ print args get it for url"""
+
+    string = string_p.split("_")
+    new_string = ""
+    for word in string:
+        new_string = new_string + " " + word
+    return new_string[1:]
+
+
+@app.route('/python')
+@app.route('/python/')
+def default_phyton():
+    """default_phyton"""
+    return ("Python is cool")
+
+
+@app.route('/python/<text>')
+def python_route(text):
+    """python_route"""
+    string = c_is_fun(text)
+    print(string)
+    return ("Python {}".format(string))
+
+
+@app.route('/number/<n>')
+def number_route(n):
+    """number_route"""
+    if '.' not in n:
+        return n + " " + "is a number"
+    pass
+
+
+@app.route('/number_template/<n>')
+def number_template(n):
+    """number_template"""
+    if '.' not in n:
+        return render_template('5-number.html', number=n)
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
