7339e9588e700f8f800baf9f9f4d6e7ed015d8ea
lemejiamo <luismejia69@gmail.com>
2021-09-12 16:38:06 -0500

    update engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,6 +7,17 @@ class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
+ 
+    @property
+    def objects(self):
+        """
+            Getter for objects
+        """
+
+        return (self.__objects)
+ 
+    def close(self):
+        reload()
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
@@ -14,7 +25,7 @@ class FileStorage:
             cls_dict = {}
             for key, value in self.__objects.items():
                 if value.__class__ == cls:
-                    cls_dict[key] = value
+                   cls_dict[key] = value
             return cls_dict
         return FileStorage.__objects
 
